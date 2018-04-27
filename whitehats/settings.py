682e1c66b8e2c64698cdc76502a0905b123ce4f4
Stephan Poetschner <stephan.poetschner@gmail.com>
2018-04-27 11:38:39 +0200

    + add template from https://themeforest.net/item/hype-app-landing-page/6671565

--- whitehats/settings.py
+++ whitehats/settings.py
@@ -122,8 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
