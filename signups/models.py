2eb13503a7056fdc0a1306cca52e23c6415ad507
Stephan Poetschner <stephan.poetschner@gmail.com>
2018-05-03 13:47:20 +0200

    + custom form rendering

--- signups/models.py
+++ signups/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class SignupUser(models.Model):
-    email = models.EmailField()
+    email = models.EmailField(
+        _('Your Email'),
+        help_text=_('We will keep you updated about new releases.'))
 
     def __str__(self):
         return self.email
