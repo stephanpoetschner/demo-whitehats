0f0d8e74a046c503dbbd16230c82095882c97d27
Stephan Poetschner <stephan.poetschner@gmail.com>
2018-05-03 12:47:16 +0200

    + add initial locale files

--- whitehats/settings.py
+++ whitehats/settings.py
@@ -107,15 +107,19 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
+LANGUAGES = (
+    'en',
+)
+
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locales'),
+)
 
 
 # Static files (CSS, JavaScript, Images)
