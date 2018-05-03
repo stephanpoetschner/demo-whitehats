940f75f1956d79fe69a2a95bb029ec63a79d6481
Stephan Poetschner <stephan.poetschner@gmail.com>
2018-05-03 15:10:02 +0200

    + rewrite to django 1.11

--- whitehats/urls.py
+++ whitehats/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.conf.urls import include, url
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('landingpage.urls')),
     url(r'^', include('signups.urls')),
+
+    url(r'^api/', include('api.urls')),
+
 ]
